e9cd6abdaa7fa52eacc4cc8b6163adc4780d6a55
Habibe Soysal <hsoysal2023@gmail.com>
2022-10-09 20:43:10 +0300

    add

--- src/test/java/stepDefinitions/AmazonStepDefinition.java
+++ src/test/java/stepDefinitions/AmazonStepDefinition.java
@@ -1,26 +1,50 @@
 package stepDefinitions;
+
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import org.junit.Assert;
-import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import pages.AmazonPage;
+import utilities.ConfigReader;
 import utilities.Driver;
+
 public class AmazonStepDefinition {
+    AmazonPage amazonPageObje= new AmazonPage();
+
     @Given("Kullanici amazon sayfasina gider")
     public void kullaniciAmazonSayfasinaGider() {
-        Driver.getDriver().get("https://amazon.com");
+        Driver.getDriver().get(ConfigReader.getProperty("amznUrl"));
     }
+
     @Then("Kullanici nutella aratir")
     public void kullaniciNutellaAratir() {
-        Driver.getDriver().findElement(By.id("twotabsearchtextbox"))
-                .sendKeys("nutella", Keys.ENTER);
+        amazonPageObje.aramaKutusu.sendKeys("Nutella"+ Keys.ENTER);
     }
-    @And("Sonuclarin nutella icerdigini test eder")
-    public void sonuclarinNutellaIcerdiginiTestEder() {
 
+    @And("Sonuclarin nutella icerdigini test eder")
+
+    public void sonuclarinNutellaIcerdiginiTestEder() {
+        String arananKElime="Nutella";
+        String actualAramaSonucu=amazonPageObje.aramaSonucElementi.getText();
+        Assert.assertTrue(actualAramaSonucu.contains(arananKElime));
     }
+
     @And("Sayfayi kapatir")
     public void sayfayiKapatir() {
+        Driver.closeDriver();
+    }
+
+    @Then("Kullanici Selenium aratir")
+    public void kullaniciSeleniumAratir() {
+        amazonPageObje.aramaKutusu.sendKeys("Selenium"+ Keys.ENTER);
+    }
+
+
+    @And("Sonuclarin Selenium icerdigini test eder")
+    public void sonuclarinSeleniumIcerdiginiTestEder() {
+        String arananKElime="Selenium";
+        String actualAramaSonucu=amazonPageObje.aramaSonucElementi.getText();
+        Assert.assertTrue(actualAramaSonucu.contains(arananKElime));
     }
 }
--- target/test-classes/features/amazonSearch.feature
+++ target/test-classes/features/amazonSearch.feature
@@ -6,3 +6,10 @@ Feature: Amazon Search
     Then Kullanici nutella aratir
     And Sonuclarin nutella icerdigini test eder
     And Sayfayi kapatir
+
+  Scenario: TC02 Kullanici amazonda Selenium aratir
+
+    Given Kullanici amazon sayfasina gider
+    Then Kullanici Selenium aratir
+    And Sonuclarin Selenium icerdigini test eder
+    And Sayfayi kapatir
